ed4633289c0cff06e280f5f4782c59747f570330
MINboooo <minminnotsemi1203@hotmail.co.jp>
2020-03-14 14:29:34 +0900

    add ビューの一部をパーシャル化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
     # @posts = Post.all
     @posts = Post.order(created_at: :desc)
@@ -21,14 +22,14 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
   def update
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "投稿を更新しました。"
     else
@@ -37,7 +38,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "投稿を削除しました。"
   end
@@ -47,5 +48,9 @@ class PostsController < ApplicationController
   def post_params # ストロングパラメータを定義する
     # params.require(:post).permit(:caption)
     params.require(:post).permit(:caption, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
