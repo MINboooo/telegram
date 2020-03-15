5485f9421de3aca5362be27f5ab7642182fded1a
MINboooo <minminnotsemi1203@hotmail.co.jp>
2020-03-15 15:28:19 +0900

    add 検索機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [ :edit, :update, :destroy]
   def index
     # @posts = Post.all
-    @posts = Post.find_newest_post(params[:page]).with_user_and_comment
+
     @comment = Comment.new
   end
 
@@ -59,6 +59,6 @@ class PostsController < ApplicationController
   end
 
   def set_post
-    @post = Post.find(params[:id])
+
   end
 end
